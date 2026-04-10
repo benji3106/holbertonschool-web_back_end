77d603b849bfce31296be128f38ee01bb10cd48b
benji3106 <benjamin3106@live.fr>
2026-04-10 10:18:51 +0200

    feat: enhance wait_n function and add test file for output verification

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
-"""Module for concurrent coroutines."""
+"""Module for running multiple coroutines concurrently."""
 import asyncio
+from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
-    """Spawn wait_random n times and return delays in ascending order."""
-    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
-    
-    results = []
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Spawn wait_random n times and return delays
+    in ascending order."""
+    delays: List[float] = []
+    tasks = [asyncio.ensure_future(wait_random(max_delay)) for _ in range(n)]
     for task in asyncio.as_completed(tasks):
-        result = await task
-        results.append(result)
-    
-    return results
+        delay = await task
+        delays.append(delay)
+    return delays
