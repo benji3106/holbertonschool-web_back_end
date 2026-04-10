41fc74413f2a52421ece5542515658b672d015a0
benji3106 <benjamin3106@live.fr>
2026-04-10 10:21:34 +0200

    feat: add measure_time module for measuring async function runtime

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Module for measuring runtime of async functions."""
+import time
+import asyncio
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """Measure average execution time per coroutine."""
+    start = time.time()
+    
+    asyncio.run(wait_n(n, max_delay))
+    
+    end = time.time()
+    
+    total_time = end - start
+    return total_time / n
