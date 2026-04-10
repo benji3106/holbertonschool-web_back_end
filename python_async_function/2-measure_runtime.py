e7fad6c01ed0418feac539755f59c19f98fefc1f
benji3106 <benjamin3106@live.fr>
2026-04-10 11:08:10 +0200

    fix: use time.perf_counter for more accurate timing in measure_time function

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -8,11 +8,8 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 def measure_time(n: int, max_delay: int) -> float:
     """Measure average execution time per coroutine."""
-    start = time.time()
-    
+    start = time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
-    
-    end = time.time()
-    
-    total_time = end - start
+    total_time = time.perf_counter() - start
+
     return total_time / n
