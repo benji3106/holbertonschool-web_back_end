0c072c5ee055d7854016c2d480f197dcdcd7df3a
benji3106 <benjamin3106@live.fr>
2026-04-20 13:55:55 +0200

    fix: enhance docstrings in Server class and dataset method

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -24,12 +24,16 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
 
 
 class Server:
+    """Server class to paginate a dataset of popular baby names.
+    """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
 
     def dataset(self) -> List[List]:
+        """Cached dataset
+        """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
