be619a52ad142103257b524e3ad9d7b514f3b857
benji3106 <benjamin3106@live.fr>
2026-04-20 11:58:02 +0200

    feat: add Server class and index_range function for pagination

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,29 @@
+from typing import List
+import csv
+
+
+def index_range(page: int, page_size: int):
+    return (page - 1) * page_size, page * page_size
+
+
+class Server:
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        start, end = index_range(page, page_size)
+        data = self.dataset()
+        return data[start:end]
