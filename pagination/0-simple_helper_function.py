15afa7466405b761f028b569ae2f8de57f8545eb
benji3106 <benjamin3106@live.fr>
2026-04-20 11:20:59 +0200

    fix: improve index_range function with type hints and docstring

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
+"""
+module with a function page 1
+"""
+from typing import Tuple
 
-def index_range(page, page_size):
-    start = (page - 1) * page_size
-    end = page * page_size
-    return start, end
+
+def index_range(page: int, page_size: int) -> Tuple:
+    """
+    the function returns the first and last indexes of the page
+    depends on the size of the page
+    """
+    result: Tuple = ((page - 1) * page_size, page * page_size)
+    return result
