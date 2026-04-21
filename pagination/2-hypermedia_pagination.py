f75d129e625cf53a8631387a67d37bf217ab4710
benji3106 <benjamin3106@live.fr>
2026-04-21 11:08:13 +0200

    fix: pycodestyle

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python3
 """Hypermedia pagination module."""
 
+
 import csv
 import math
 from typing import Dict, List, Tuple, Optional
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """Return start and end index for the pagination parameters."""
+    """
+    Return start and end index for the pagination parameters.
+    """
     return (page - 1) * page_size, page * page_size
 
 
 class Server:
-    """Server class to paginate a database of popular baby names."""
+    """
+    Server class to paginate a database of popular baby names.
+    """
 
     DATA_FILE = "Popular_Baby_Names.csv"
 
@@ -20,7 +25,9 @@ class Server:
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset."""
+        """
+        Cached dataset.
+        """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -30,7 +37,9 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Return a page of the dataset."""
+        """
+        Return a page of the dataset.
+        """
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
@@ -39,7 +48,8 @@ class Server:
         return data[start:end]
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Optional[object]]:
-        """Return a dictionary with hypermedia pagination information."""
+        """Return a dictionary with hypermedia pagination information.
+        """
         data = self.get_page(page, page_size)
         total_items = len(self.dataset())
         total_pages = math.ceil(total_items / page_size)
