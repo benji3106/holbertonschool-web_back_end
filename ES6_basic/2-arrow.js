65c7df99cab0f1dc3c56de24d87cff41a255678b
benji3106 <benjamin3106@live.fr>
2026-04-27 15:29:20 +0200

    fix: use 'self' reference in addNeighborhood function to avoid context issues

--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -1,8 +1,9 @@
 export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
+  const self = this;
   this.addNeighborhood = function add(newNeighborhood) {
-    this.sanFranciscoNeighborhoods.push(newNeighborhood);
-    return this.sanFranciscoNeighborhoods;
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
   };
 }
