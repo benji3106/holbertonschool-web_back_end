93a02cf15c5ee63c4b0447664e7559ac28dabab4
benji3106 <benjamin3106@live.fr>
2026-04-27 15:21:34 +0200

    fix: remove unnecessary variable declaration in getNeighborhoodsList function

--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -1,7 +1,6 @@
 export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
-  const self = this;
   this.addNeighborhood = function add(newNeighborhood) {
     this.sanFranciscoNeighborhoods.push(newNeighborhood);
     return this.sanFranciscoNeighborhoods;
