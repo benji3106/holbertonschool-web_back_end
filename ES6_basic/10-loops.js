4e271d4282b4aec0d06f52b4b0802aaac929cfa1
benji3106 <benjamin3106@live.fr>
2026-04-28 14:16:07 +0200

    fix: revert loop to use index variable for appending string to array values

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,6 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (let [idx, value] of array.entries()) {
-    array[idx] = appendString + value;
+  let i = 0;
+
+  for (const value of array) {
+    array[i] = appendString + value;
+    i++;
   }
 
   return array;
