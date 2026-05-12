1acc49392bc95401952b219e2e119ce398a7e784
benji3106 <benjamin3106@live.fr>
2026-05-12 13:45:21 +0200

    fix: output formatting for user name and handle graceful shutdown

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,10 +1,9 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.setEncoding('utf8');
 process.stdin.on('data', (data) => {
-  process.stdout.write(`Your name is: ${data}`);
+  process.stdout.write(`Your name is: ${data.toString()}`);
 });
 
-process.on('SIGINT', () => {
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
-  process.exit();
 });
